1bf7e17580393d6f9813de9d0d2e2b305a120a18
Ad4mJ <>
2014-08-22 14:29:14 +0100

    signalRKnob

---
+++ SignalRChat/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+namespace SignalRChat
+{
+    public class ChatHub : Hub
+    {
+        public void Change(int value)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastChange(value);
+        }
+
+        public void Release(int value)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastRelease(value);
+        }
+    }
+}
